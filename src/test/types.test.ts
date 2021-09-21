52a0d51888a955642c3b42bcd3dace8a0b86a528
Foxhoundn <8861481+Foxhoundn@users.noreply.github.com>
2021-09-21 11:29:51 +0200

    Type testing

---
+++ src/test/types.test.ts
@@ -0,0 +1,16 @@
+import { ControlFlowGraph } from '../control-flow';
+import { Def, parse } from '../python-parser';
+
+describe('TypeTesting', () => {
+  it('tests types in code', () => {
+    let ast = parse(['def foo(n):', "    bar: str = 'foobar'"].join('\n'));
+
+    console.log(ast);
+
+    // expect(ast.code).toHaveLength(1);
+    // expect(ast.code[0].type).toBe("def");
+    // const cfg = new ControlFlowGraph(ast.code[0] as Def);
+    // expect(cfg.blocks).toBeDefined();
+    // expect(cfg.blocks).toHaveLength(6);
+  });
+});
